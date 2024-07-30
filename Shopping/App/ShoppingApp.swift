8bdd70e942f5676ef6ac94503c02f547f778d650
Berkay Sancar <sancarberkay@gmail.com>
2024-07-30 17:55:34 +0300

    adds appearences

--- Shopping/App/ShoppingApp.swift
+++ Shopping/App/ShoppingApp.swift
@@ -9,9 +9,27 @@ import SwiftUI
 
 @main
 struct ShoppingApp: App {
+    
     var body: some Scene {
         WindowGroup {
             SplashView()
         }
+    }
+    
+    init() {
+        pageControlAppearence()
+        navBarAppearance()
+    }
+    
+    private func pageControlAppearence() {
+        UIPageControl.appearance().currentPageIndicatorTintColor = .appOrange
+        UIPageControl.appearance().pageIndicatorTintColor = UIColor.black.withAlphaComponent(0.2)
+    }
+    
+    private func navBarAppearance() {
+        let appearance = UINavigationBarAppearance()
+        appearance.titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.appOrange]
+        UIBarButtonItem.appearance().tintColor = UIColor.appOrange
+        UINavigationBar.appearance().standardAppearance = appearance
     }
 }
